ae8c2f63746f6f61199b56594b1ddaa09ef0c13a
Bob Cowling <rcowling@mtu.edu>
2022-08-01 07:30:33 -0400

    add modal

---
+++ index.ts
@@ -0,0 +1,7 @@
+import { defineCustomElements } from '@ionic/core/loader';
+
+import { createAnimation } from '@ionic/core';
+
+defineCustomElements();
+
+(window as any).createAnimation = createAnimation;
