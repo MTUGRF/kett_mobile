837f17654af1aed027580265eb4edee4a6ac670e
MTU Geospatial Research Facility <101424237+MTUGRF@users.noreply.github.com>
2023-03-06 14:59:31 -0500

    Delete index.ts

--- index.ts
+++
@@ -1,7 +0,0 @@
-import { defineCustomElements } from '@ionic/core/loader';
-
-import { createAnimation } from '@ionic/core';
-
-defineCustomElements();
-
-(window as any).createAnimation = createAnimation;
